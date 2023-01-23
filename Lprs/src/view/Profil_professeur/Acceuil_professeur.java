c734544879842cda5f833a54788f71409a1193ca
imen23 <90242060+Azerty233@users.noreply.github.com>
2023-01-23 11:02:21 +0100

    Create Acceuil professeur

---
+++ Lprs/src/view/Profil_professeur/Acceuil_professeur.java
@@ -0,0 +1,43 @@
+package view.Profil_professeur;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Acceuil_professeur {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Acceuil_professeur window = new Acceuil_professeur();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Acceuil_professeur() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
