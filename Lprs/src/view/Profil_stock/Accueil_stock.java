83ef7cf2fbf43f3dc1f90304171c8a9aa8e6a63d
bambaibrahim02 <ibrayoman02@gmail.com>
2023-01-23 11:12:29 +0100

    Création profil stock et profil admin

---
+++ Lprs/src/view/Profil_stock/Accueil_stock.java
@@ -0,0 +1,43 @@
+package view.Profil_stock;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Accueil_stock {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Accueil_stock window = new Accueil_stock();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Accueil_stock() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
