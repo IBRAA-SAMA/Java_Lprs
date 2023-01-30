eacaa335d531c9e10d02ab311d83e9b546073b66
imen23 <90242060+Azerty233@users.noreply.github.com>
2023-01-30 09:42:13 +0100

    Formulaire Proffeseur
    Formulaire d'inscription spécifique au professeur

---
+++ Lprs/src/view/Profil_admin/Formulaire_professeur.java
@@ -0,0 +1,42 @@
+package view.Profil_admin;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Formulaire_professeur {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Formulaire_professeur window = new Formulaire_professeur();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Formulaire_professeur() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
